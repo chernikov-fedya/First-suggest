54fdb1b29d90f596c52a8dccceb145dd7460838b
curlyettin <chernikov-fedya@mai.ru>
2023-08-07 15:17:35 +0300

    Half GUI

--- post_work.py
+++ post_work.py
@@ -64,11 +64,75 @@ def sender():
             name_group = vk.groups.getById(group_id= i)[0]
             print(e, name_group['name'])
 
-layout = [[sg.Button('Новое число',enable_events=True, key='-FUNCTION-', font='Helvetica 16')],
-        # затем делаем текст
-        [sg.Text('Результат:', size=(25, 1), key='-text-', font='Helvetica 16')]]
-# рисуем окно
-window = sg.Window('Спаммер', layout, size=(350,100))
 
+sg.theme('DarkAmber')
+# # Цикл для обработки "событий" и получения "значений" входных данных
+# while True:
+#     event, values = window.read()
+#     if event == sg.WIN_CLOSED or event == 'Отмена':
+#         break
+#     print('Молодец, ты справился с вводом', values[0])
+
+file_list_column = [
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+    [
+        sg.Text("ВыБери вещь"),
+        sg.Button('Начать')
+    ]
+]
+
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
+
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+
+window = sg.Window("Спаммер в предложку", layout)
+
+# Run the Event Loop
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    # Folder name was filled in, make a list of files in the folder
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE-"].update(filename=filename)
+
+        except:
+            pass
 
 window.close()
